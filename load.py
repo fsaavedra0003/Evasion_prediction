974d90469bebdc9b08e824901e4380b355f17445
fsaavedra0003 <68877794+fsaavedra0003@users.noreply.github.com>
2023-10-31 15:55:57 -0300

    Add files via upload

---
+++ load.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+
+# CSV file with history of evations which initial needed.
+csv_file = 'evasion.csv'
+
+#  CSV gets into a pandas DataFrame
+df = pd.read_csv(csv_file)
+
+# Print the Data frame with the 
+print(df)
