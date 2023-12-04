74cc033702cdb3bcf81050e7f5faeec9e1c6983b
fsaavedra0003 <68877794+fsaavedra0003@users.noreply.github.com>
2023-12-04 18:34:31 +0100

    Update load.py

--- load.py
+++ load.py
@@ -1,12 +1,52 @@
+#Imported the corresponding libraries 
+
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
 
 
 # CSV file with history of evations
 
 csv_file = 'evasion.csv'
 
-#  CSV gets into a pandas DataFrame
+#  Load CSV gets into a pandas DataFrame
 df = pd.read_csv(csv_file)
 
 # Print the CSV as Pandas Data Frame
 print(df)
+
+
+# Load the corresponding columns from  CSV  'feature' and 'target'
+X = data[['feature']]
+y = data['target']
+
+# This is the logic to Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Create a linear regression model
+model = LinearRegression()
+
+# Train the model
+model.fit(X_train, y_train)
+
+# Make predictions on the test set
+y_pred = model.predict(X_test)
+
+# Evaluate the acuracy of model using  MSE (mean squared error).
+mse = mean_squared_error(y_test, y_pred)
+
+
+# Print the results.
+print(f'Mean Squared Error: {mse}')
+
+
+
+# Plot the regression line using Matplolib of the predicted values
+plt.scatter(X_test, y_test, color='black')
+plt.plot(X_test, y_pred, color='blue', linewidth=3)
+plt.xlabel('Feature')
+plt.ylabel('Target')
+plt.title('Linear Regression Model')
+plt.show()
